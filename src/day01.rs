f20f7ba4fac4c5ae296c853bf785b60b3c4e09a6
Ritoban Roy-Chowdhury <ritobanrc@gmail.com>
2020-06-02 17:04:32 -0700

    day 1

--- src/day01.rs
+++ src/day01.rs
@@ -1,4 +1,49 @@
+use anyhow::{anyhow, Result};
+
 #[test]
-fn day01_main() -> i64 {
-    let input = load_input(1);
+pub fn day01_main() -> Result<()> {
+    use crate::load_input;
+    let mut input = load_input(1)?;
+    input.pop(); // remove newline
+
+    assert_eq!(part1_solution(&input)?, 74);
+    assert_eq!(part2_solution(&input)?, 1795);
+
+    Ok(())
+}
+
+fn part1_solution(input: &[u8]) -> Result<i64> {
+    let mut count = 0;
+    for &c in input {
+        match c {
+            b'(' => {
+                count += 1;
+            }
+            b')' => {
+                count -= 1;
+            }
+            _ => return Err(anyhow!("Unexpected character: {}", c)),
+        }
+    }
+    Ok(count)
+}
+
+fn part2_solution(input: &[u8]) -> Result<usize> {
+    let mut count = 0;
+    for (i, &c) in input.iter().enumerate() {
+        match c {
+            b'(' => {
+                count += 1;
+            }
+            b')' => {
+                count -= 1;
+            }
+            _ => return Err(anyhow!("Unexpected character: {}", c)),
+        }
+
+        if count < 0 {
+            return Ok(i + 1); // answer is 1 indexed
+        }
+    }
+    Err(anyhow!("Did not enter basement"))
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
+#[allow(dead_code)]
 mod day01;
 
-use std::io::{self, BufRead, BufReader};
+use std::io;
 
-fn load_input(day: usize) -> io::Result<impl BufRead> {
-    use std::fs::File;
+pub fn load_input(day: usize) -> io::Result<Vec<u8>> {
+    use std::fs::read;
     use std::path::PathBuf;
     let path: PathBuf = ["input", &format!("day{:02}.txt", day)].iter().collect();
-    let file = File::open(path)?;
-    Ok(BufReader::new(file))
+    read(path)
 }
