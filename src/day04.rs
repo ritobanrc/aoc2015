6edbf26e376e88e34ccee0a9b6ad60d3c07f94e9
Ritoban Roy-Chowdhury <ritobanrc@gmail.com>
2020-06-02 18:37:15 -0700

    day 4 complete

---
+++ src/day04.rs
@@ -0,0 +1,29 @@
+use md5;
+
+#[test]
+fn day04_main() {
+    const INPUT: &'static str = "ckczppom";
+
+    assert_eq!(117946, part1_solution(INPUT));
+    assert_eq!(3938038, part2_solution(INPUT));
+}
+
+fn crack_md5(input: &str, starts_with: &str) -> usize {
+    let mut ans = 1;
+    loop {
+        let data = format!("{}{}", input, ans);
+        let digest = format!("{:x}", md5::compute(data));
+        if digest.starts_with(starts_with) {
+            return ans;
+        }
+        ans += 1;
+    }
+}
+
+fn part1_solution(input: &str) -> usize {
+    crack_md5(input, "00000")
+}
+
+fn part2_solution(input: &str) -> usize {
+    crack_md5(input, "000000")
+}
